d18790623aba0ca8c49e98340320a2486a6128f5
Diego A. Ribera <diegoriberarodriguez@gmail.com>
2025-09-11 15:46:51 -0400

    Config: conexión PostgreSQL Render y migraciones

--- VercelDeploy/settings.py
+++ VercelDeploy/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,16 +78,11 @@ WSGI_APPLICATION = 'VercelDeploy.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Productos',
-        'USER': 'postgres',
-        'PASSWORD': '11324499',
-        'HOSTS': 'localhosts',
-        'PORT': '5432',
 
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config("DATABASE_URL")
+    )
 }
 
 
