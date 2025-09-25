378fa8d8f50a07dd8f8fa35a424e44035e4f76d3
surubikevin-lab <surubikevin@gmail.com>
2025-09-25 14:24:20 -0400

    cambio

--- VercelDeploy/urls.py
+++ VercelDeploy/urls.py
@@ -15,15 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse("✅ Django está funcionando en Vercel")
-
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", home),  # Ruta principal
+    path('admin/', admin.site.urls),
+    path('', include('Home.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
