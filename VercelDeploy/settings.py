f779989f0a3deb07395c2d6198a8c001e8f74c76
surubikevin-lab <surubikevin@gmail.com>
2025-09-24 23:24:18 -0400

    cambio

--- VercelDeploy/settings.py
+++ VercelDeploy/settings.py
@@ -64,19 +64,24 @@ TEMPLATES = [
 # WSGI
 WSGI_APPLICATION = 'VercelDeploy.wsgi.application'
 
+IS_VERCEL = os.environ.get("VERCEL") is not None
 
-# Usar DATABASE_URL siempre que exista
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',  # Fallback para desarrollo
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if IS_VERCEL:
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
-# Forzar PostgreSQL si estamos en Vercel
-if os.environ.get('VERCEL'):
-    DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
 # Password validators
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
